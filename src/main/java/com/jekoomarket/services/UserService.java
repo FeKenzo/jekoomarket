2d2373bd46e867b1e09fe26b9a8a2255fea5b9f3
João Vicentim <113844703+JoaoVicentim@users.noreply.github.com>
2025-06-03 20:56:52 -0300

    Update UserService.java

--- src/main/java/com/jekoomarket/services/UserService.java
+++ src/main/java/com/jekoomarket/services/UserService.java
@@ -7,14 +7,10 @@ import java.util.Optional;
 
 public interface UserService {
     void save(User user);
-
     List<User> findAll();
-
     Optional<User> findById(Long id);
-
     Optional<User> findByEmail(String email);
-
     boolean existsByEmail(String email);
-
     void deleteById(Long id);
+    long countAllUsers(); // New method
 }
