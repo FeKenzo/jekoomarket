9c1f5265ab2aef69a758d9d1347f50212d15d1dc
João Vicentim <113844703+JoaoVicentim@users.noreply.github.com>
2025-06-03 21:23:51 -0300

    Update OrderService.java

--- src/main/java/com/jekoomarket/services/OrderService.java
+++ src/main/java/com/jekoomarket/services/OrderService.java
@@ -4,9 +4,11 @@ import com.jekoomarket.models.Order;
 import com.jekoomarket.models.Product;
 import com.jekoomarket.models.User;
 
+import java.util.List;
 import java.util.Map;
 
 public interface OrderService {
     Order placeOrder(User user, Product product, int quantity, Map<String, String> deliveryAddress, String paymentMethod);
     long countAllOrders(); // New method
+    List<Order> findByUser(User user);
 }
